481d52c2a2dba050170646df5687ab2f5e0cd1e5
Mario Manno <mario.manno@suse.com>
2018-12-06 12:32:39 +0100

    update nvim, git verbose=true, use command instead which

--- update-skel.sh
+++ update-skel.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Description: helps updating skel directory and lists
 
+set -e -o pipefail
+
 print_new_files () {
   # add new files to installer
   echo "# [ ] new files in skel (not in lists/)"
-  find skel -type f | sed 's/skel\///' | while read f; do
+  find skel -type f | sed 's/skel\///' | sort | while read f; do
     echo "$f" | egrep -q "\.gitkeep|.gitignore$" || (grep -q "^$f$" lists/*.lst || echo "$f")
   done
   echo
@@ -12,7 +14,7 @@ print_new_files () {
 
 remove_old_files() {
   # remove old files from installer
-  echo "# [ ] files no longer in skel, but in lists/"
+  echo "# [ ] files no longer in skel, but in tarball lists/"
   for list in lists/install.lst lists/lite.lst; do
     grep -hv "^-" $list | while read f; do
       if [ ! -e skel/$f ]; then
@@ -29,10 +31,10 @@ print_existing_diff() {
   # find all files from . which changed in $HOME
   echo "# [ ] files from skel/ that changed in $HOME"
   cd skel
-  find . -type f -o -type l | \
+  find -L . -type f | \
     grep -v $0 | \
-    grep -v '.vim/bundle/vundle' |  \
     grep -v '.gitkeep' | \
+    sort | \
     ruby -ne 'h=ENV["HOME"];f=$_.chomp.gsub(/\A\.\//,"");$_=%x(diff -q "#{f}" #{h}/#{f} 2>&1);if /differ/; puts "vi -d skel/#{f} #{h}/#{f}"; elsif /diff:.*#{h}.*: No such file/; puts "cp skel/#{f} #{h}/#{f}"; end'
 
   # | egrep -v 'vi -d (skel/.bash_profile|skel/.profile)'
@@ -41,4 +43,5 @@ print_existing_diff() {
 
 print_new_files
 remove_old_files
-print_existing_diff # perl deploy.pl -D
+print_existing_diff
+# perl deploy.pl -D
