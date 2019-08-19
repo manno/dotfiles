ce39f9501ca0714611ba1bae4d8749d44fd14221
Mario Manno <root@localhost>
2019-08-20 01:01:31 +0200

    Add gsettings, terminator and firefox setup scripts

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Install
 --------------
 
    ./install.sh
+
+
+Other
+--------------
+
+* `firefox-nightly-setup.sh`: add firefox nightly to Gnome
+* `gsettings-keys.sh`: my Gnome keybindings
+* `update-skel.sh`: compare existing dotfiles to files in skel
---
+++ gsettings-keys.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+function dump() {
+  gsettings list-recursively org.gnome.desktop.wm.keybindings
+  gsettings list-recursively org.gnome.mutter.keybindings
+  gsettings list-recursively org.gnome.shell.keybindings
+  gsettings list-recursively org.gnome.mutter.wayland.keybindings
+}
+
+function org.gnome.shell.keybindings() {
+  gsettings set org.gnome.shell.keybindings "$1" "$2"
+}
+
+function org.gnome.desktop.wm.keybindings() {
+  gsettings set org.gnome.desktop.wm.keybindings "$1" "$2"
+}
+
+function org.gnome.mutter.keybindings() {
+  gsettings set org.gnome.mutter.keybindings "$1" "$2"
+}
+
+dump > keybindings.lst
+
+dconf write /org/gnome/terminal/legacy/keybindings/copy  '"<Super>c"'
+dconf write /org/gnome/terminal/legacy/keybindings/paste '"<Super>v"'
+
+org.gnome.shell.keybindings switch-to-application-1 []
+org.gnome.shell.keybindings switch-to-application-2 []
+org.gnome.shell.keybindings switch-to-application-3 []
+org.gnome.shell.keybindings switch-to-application-4 []
+org.gnome.shell.keybindings switch-to-application-5 []
+org.gnome.shell.keybindings switch-to-application-6 []
+org.gnome.shell.keybindings switch-to-application-7 []
+org.gnome.shell.keybindings switch-to-application-8 []
+org.gnome.shell.keybindings switch-to-application-9 []
+org.gnome.shell.keybindings toggle-message-tray []
+org.gnome.shell.keybindings toggle-application-view []
+
+org.gnome.desktop.wm.keybindings maximize []
+org.gnome.desktop.wm.keybindings minimize []
+org.gnome.desktop.wm.keybindings unmaximize []
+
+org.gnome.mutter.keybindings toggle-tiled-right "['<Alt><Super>Right']"
+org.gnome.mutter.keybindings toggle-tiled-left "['<Alt><Super>Left']"
