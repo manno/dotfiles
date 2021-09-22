45e1bf9b5c34f3dbb0b2c2e3133dccff7ce98aaf
Mario Manno <mmanno@suse.com>
2021-09-22 12:06:32 +0200

    Add readme/contrib

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Install
 --------------
 
    chezmoi init manno
+   chezmoi git -- remote set-url origin --push git@github.com:manno/dotfiles.git
---
+++ contrib/set_caps_lock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
