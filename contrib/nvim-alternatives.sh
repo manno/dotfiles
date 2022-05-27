38d58752a44558288d6981c8f52a6b36b3d8d0ca
Mario Manno <mario.manno@suse.com>
2022-05-27 11:07:09 +0200

    updates from tumbleweed

--- contrib/nvim-alternatives.sh
+++ contrib/nvim-alternatives.sh
@@ -1,2 +1,11 @@
-sudo update-alternatives --install "/usr/bin/vim" "vim" "/usr/bin/nvim" 9000
+#!/bin/sh
+
+[ -x "$(command -v update-alternatives)" ] || exit 0
+
+[ -e /usr/local/bin/nvim ] && sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
+[ -x "$(command -v nvim)" ] || exit 0
+
 sudo update-alternatives --install "/usr/bin/vi" "vi" "/usr/bin/nvim" 9000
+sudo update-alternatives --install "/usr/bin/vim" "vim" "/usr/bin/nvim" 9000
+sudo update-alternatives --set vi "/usr/bin/nvim"
+sudo update-alternatives --set vim "/usr/bin/nvim"
