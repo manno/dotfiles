1e5016577eb7228e735cc5d3af8e223ff6e68cd8
Mario Manno <mmanno@suse.com>
2024-03-19 17:03:01 +0100

    switch to lazy.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,70 +1,67 @@
 -- vim: ts=2 sw=2
--- https://github.com/wbthomason/packer.nvim
 -- https://github.com/nanotee/nvim-lua-guide
 
 local silentOpt = { silent = true }
 local allOpt = { noremap = true, silent = true, nowait = true }
 
--- Packer
--- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-local packer_bootstrap
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
--- https://github.com/wbthomason/packer.nvim#specifying-plugins
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require("lazy").setup({
+  { 'wbthomason/packer.nvim' },
 
   -- Parsers, replaces vim-polyglot
   -- TSInstall ft
-  use {
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = "all",
-        ignore_install = { "phpdoc" },
-        highlight = {
-          enable = true,                 -- false will disable the whole extension
-          -- disable = { "c", "rust" },  -- list of language that will be disabled
-        },
-        textobjects = {
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]]"] = "@function.outer",
-              ["]m"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]["] = "@function.outer",
-              ["]M"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[["] = "@function.outer",
-              ["[m"] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[]"] = "@function.outer",
-              ["[M"] = "@class.outer",
-            },
+    build = ':TSUpdate',
+    opts = {
+      -- ensure_installed = "all",
+      -- ignore_install = { "phpdoc" },
+      ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'yaml' },
+      highlight = {
+        enable = true,                 -- false will disable the whole extension
+        -- disable = { "c", "rust" },  -- list of language that will be disabled
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]]"] = "@function.outer",
+            ["]m"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]["] = "@function.outer",
+            ["]M"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[["] = "@function.outer",
+            ["[m"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[]"] = "@function.outer",
+            ["[M"] = "@class.outer",
           },
         },
-      }
+      },
+    },
+    config = function(_, opts)
+      require 'nvim-treesitter.configs'.setup(opts)
     end
-  }
-  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-  use { 'nvim-treesitter/nvim-treesitter-context' }
+  },
+  { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  { 'nvim-treesitter/nvim-treesitter-context' },
 
   -- Languageservers
   -- run :CocConfig to add language servers, e.g.
@@ -75,10 +72,10 @@ return require('packer').startup(function(use)
   -- run :CocUpdate
   -- run :CocInstall ft
   -- CocInstall coc-diagnostic
-  use {
+  {
     'neoclide/coc.nvim',
     branch = 'release',
-    run = ':CocInstall coc-go',
+    build = ':CocInstall coc-go',
     config = function()
       vim.g.coc_global_extensions = { 'coc-json', 'coc-diagnostic', 'coc-tsserver', 'coc-go', 'coc-solargraph', 'coc-clangd', 'coc-yaml', 'coc-lua' }
 
@@ -167,16 +164,16 @@ return require('packer').startup(function(use)
         end
       })
     end
-  }
+  },
 
   -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    requires = {
-            { 'nvim-lua/plenary.nvim' },
-            { 'nvim-telescope/telescope-fzf-native.nvim',
-                 run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-            { "davvid/telescope-git-grep.nvim", tag = "v1.0.2" },
+  {
+    'nvim-telescope/telescope.nvim', version = '0.1.6',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+      { "davvid/telescope-git-grep.nvim", version = "v1.0.2" },
     },
     config = function()
       require("telescope").setup({
@@ -207,131 +204,151 @@ return require('packer').startup(function(use)
       vim.keymap.set('n', '<leader>j', builtin.jumplist, {})
       vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
     end
-  }
+  },
 
-  use 'nvim-telescope/telescope-symbols.nvim'
+  {'nvim-telescope/telescope-symbols.nvim'},
 
   -- Status line
-  use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
-
-  use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
-  use { 'romgrk/barbar.nvim', config = function()
+  {
+    'romgrk/barbar.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'lewis6991/gitsigns.nvim' },
+    config = function()
       vim.keymap.set('', '<leader>w', ':BufferWipeout<cr>')
       vim.keymap.set('', '<C-n>', ':BufferNext<cr>')
       vim.keymap.set('', '<C-p>', ':BufferPrevious<cr>')
-  end }
+    end
+  },
 
-  use { 'hoob3rt/lualine.nvim', config = function()
-    require("lualine").setup({
-      options = {
-        theme = 'auto',
-        globalstatus = true,
-      },
-      sections = {
-        lualine_b = {'windows', 'diff', 'diagnostics'},
-        lualine_c = {
-          {
-            'filename',
-            file_status = true,
-            newfile_status = true,
-            path = 1,
-            shorting_target = 20,
-          }
+  {
+    'hoob3rt/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = 'auto',
+          globalstatus = true,
         },
-        lualine_y = {'searchcount', 'progress'},
-      }
-    })
-  end }
-
-  -- use {'folke/trouble.nvim', config = {
-  --   require("trouble").setup{}
-  --   -- how is that different from CocList diagnostics
-  --   -- nnoremap <silent><nowait> <space>D  <cmd>TroubleToggle lsp_workspace_diagnostics<cr>
-  --   }
-  -- }
+        sections = {
+          lualine_b = {'windows', 'diff', 'diagnostics'},
+          lualine_c = {
+            {
+              'filename',
+              file_status = true,
+              newfile_status = true,
+              path = 1,
+              shorting_target = 20,
+            }
+          },
+          lualine_y = {'searchcount', 'progress'},
+        }
+      })
+    end
+  },
 
   -- NerdCommenter
-  use { 'scrooloose/nerdcommenter', config = function()
-    vim.g.NERDSpaceDelims = 1
-  end }
+  {
+    'scrooloose/nerdcommenter',
+    config = function()
+      vim.g.NERDSpaceDelims = 1
+    end
+  },
 
   -- Autocompletion
-  use { 'github/copilot.vim', ft = {'ruby', 'go', 'js', 'sh', 'lua', 'vim', 'yaml'} }
+  { 'github/copilot.vim', ft = {'ruby', 'go', 'js', 'sh', 'lua', 'vim', 'yaml'} },
 
   -- Colorschemes
-  use { 'jonathanfilip/vim-lucius', opt = true }
-  use { 'tomasr/molokai', opt = true }
-  use { 'noahfrederick/vim-hemisu', opt = true }
-  use { 'endel/vim-github-colorscheme', opt = true }
-  --use 'chriskempson/vim-tomorrow-theme'
-  --use 'iCyMind/NeoSolarized'
-  use { 'TroyFletcher/vim-colors-synthwave', opt = true }
-  --use 'drewtempelmeyer/palenight.vim'
-  --use {'embark-theme/vim', { as = 'embark' }}
-  --use 'sainnhe/sonokai'
-  use { 'folke/tokyonight.nvim', config = function()
-    -- vim.cmd[[colorscheme tokyonight]]
-  end }
+  { 'jonathanfilip/vim-lucius', lazy = true },
+  { 'tomasr/molokai', lazy = true },
+  { 'noahfrederick/vim-hemisu', lazy = true },
+  { 'endel/vim-github-colorscheme', lazy = true },
+  --'chriskempson/vim-tomorrow-theme'
+  --'iCyMind/NeoSolarized'
+  { 'TroyFletcher/vim-colors-synthwave', lazy = true },
+  --'drewtempelmeyer/palenight.vim'
+  --{'embark-theme/vim', { name = 'embark' }}
+  --'sainnhe/sonokai'
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      -- vim.cmd[[colorscheme tokyonight]]
+    end
+  },
 
-  use { 'sontungexpt/witch', config = function(_, opts)
-    --require("witch").setup(opts)
-    --vim.cmd[[colorscheme witch]]
-  end }
+  {
+    'sontungexpt/witch',
+    config = function(_, opts)
+      --require("witch").setup(opts)
+      --vim.cmd[[colorscheme witch]]
+    end
+  },
 
-  use { 'binhtran432k/dracula.nvim', config = function(_, opts)
-    require("dracula").setup(opts)
-    vim.cmd[[colorscheme dracula]]
-  end }
+  {
+    'binhtran432k/dracula.nvim',
+    config = function(_, opts)
+      require("dracula").setup(opts)
+      vim.cmd[[colorscheme dracula]]
+    end
+  },
 
   -- Tmux integration
-  use { 'edkolev/tmuxline.vim', opt = true }
+  { 'edkolev/tmuxline.vim', lazy = true },
 
   -- Readline style insertion
-  use 'tpope/vim-rsi'
+  {'tpope/vim-rsi'},
 
   -- Format SQL
-  use 'vim-scripts/SQLUtilities'
+  {'vim-scripts/SQLUtilities'},
 
   -- Surround - sa%" sa$' saE" srb" sr"' sd"
-  --use 'machakann/vim-sandwich'
-  use { 'kylechui/nvim-surround', config = function() require("nvim-surround").setup({}) end }
+  --'machakann/vim-sandwich'
+  {
+    'kylechui/nvim-surround',
+    config = function() require("nvim-surround").setup({}) end
+  },
 
   -- Spider cursor movement
-  use { "chrisgrieser/nvim-spider" }
+  { "chrisgrieser/nvim-spider" },
 
   -- Vim ruby
-  use { 'tpope/vim-bundler', ft = {'ruby'} }
-  use { 'tpope/vim-rake', ft = {'ruby'} }
-  use { 'tpope/vim-rails', ft = {'ruby'} }
-  use { 'janko-m/vim-test', config = function()
-    vim.g['test#strategy'] = "neovim"
-    vim.keymap.set("n", "<F3>", ":TestFile<CR>")
-  end }
+  { 'tpope/vim-bundler', ft = {'ruby'} },
+  { 'tpope/vim-rake', ft = {'ruby'} },
+  { 'tpope/vim-rails', ft = {'ruby'} },
+  {
+    'janko-m/vim-test',
+    config = function()
+      vim.g['test#strategy'] = "neovim"
+      vim.keymap.set("n", "<F3>", ":TestFile<CR>")
+    end
+  },
 
   -- Open files at line
-  use 'manno/file-line'
+  {'manno/file-line'},
 
-  use { 'zerowidth/vim-copy-as-rtf', cond = function() return vim.fn.has('mac') end }
+  {
+    'zerowidth/vim-copy-as-rtf',
+    cond = function() return vim.fn.has('mac') end
+  },
 
-  use "almo7aya/openingh.nvim"
+  {"almo7aya/openingh.nvim"},
 
   -- Markdown preview
-  use { 'davinche/godown-vim', ft = {'markdown'} }
+  { 'davinche/godown-vim', ft = {'markdown'} },
 
   -- Git
-  use { 'tpope/vim-fugitive', config = function()
-    -- vim.keymap.set("", "<leader>G", ":Ggrep <C-R><C-W> ':(exclude)*fake*'<CR>")
-  end }
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      -- vim.keymap.set("", "<leader>G", ":Ggrep <C-R><C-W> ':(exclude)*fake*'<CR>")
+    end
+  },
 
-  use 'airblade/vim-gitgutter'
+  {'airblade/vim-gitgutter'},
 
   -- Snippets
-  use 'rafamadriz/friendly-snippets'
-  use { 'L3MON4D3/LuaSnip', config = function() require("luasnip.loaders.from_vscode").lazy_load() end }
+  {'rafamadriz/friendly-snippets'},
+  {
+    'L3MON4D3/LuaSnip',
+    config = function() require("luasnip.loaders.from_vscode").lazy_load() end
+  },
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+})
