891766c40bb05f7873c8192722307bde7ff87609
Mario Manno <mario.manno@gmail.com>
2025-03-28 18:07:21 +0100

    CodeCompanion

--- dot_config/nvim/lua/plugins/assistance.lua
+++ dot_config/nvim/lua/plugins/assistance.lua
@@ -3,47 +3,47 @@
 
 return {
 
-  -- {
-  --   "olimorris/codecompanion.nvim",
-  --   config = true,
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   -- cond = function()
-  --   --   return os.getenv("GEMINI_API_KEY") ~= "" and os.getenv("GEMINI_API_KEY") ~= nil
-  --   -- end,
-  --   opts = {
-  --     adapters = {
-  --       gemini = function()
-  --         return require("codecompanion.adapters").extend("gemini", {
-  --           env = {
-  --             api_key = function()
-  --               return os.getenv("GEMINI_API_KEY")
-  --             end
-  --           },
-  --         })
-  --       end,
-  --       llama3 = function()
-  --         return require("codecompanion.adapters").extend("ollama", {
-  --           name = "llama3",
-  --           schema = {
-  --             model = { default = "llama3:latest", },
-  --             num_ctx = { default = 16384, },
-  --             num_predict = { default = -1, },
-  --           },
-  --         })
-  --       end,
-  --     },
-  --     strategies = {
-  --       chat = {
-  --         adapter = "llama3",
-  --       },
-  --       inline = {
-  --         adapter = "llama3",
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "olimorris/codecompanion.nvim",
+    config = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- cond = function()
+    --   return os.getenv("GEMINI_API_KEY") ~= "" and os.getenv("GEMINI_API_KEY") ~= nil
+    -- end,
+    opts = {
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = function()
+                return os.getenv("GEMINI_API_KEY")
+              end
+            },
+          })
+        end,
+        llama3 = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            name = "llama3",
+            schema = {
+              model = { default = "llama3:latest", },
+              num_ctx = { default = 16384, },
+              num_predict = { default = -1, },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+      },
+    },
+  },
 
 }
