84feec491ff4fff268be71b22d002770316568c2
Mario Manno <mmanno@suse.com>
2025-05-14 16:53:34 +0200

    fzf HOMEBREW_PREFIX

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -10,13 +10,15 @@ if [[ ! "$MANPATH" == */$HOME/.fzf/man* && -d "/home/mm/.fzf/man" ]]; then
   export MANPATH="$MANPATH:/$HOME/.fzf/man"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+# Auto-completion + Key bindings
+# ------------------------------
+if [ -d "$HOME/.fzf/shell" ]; then
+    [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+    source "$HOME/.fzf/shell/key-bindings.zsh"
+elif [ -d "$HOMEBREW_PREFIX/opt/fzf/shell" ]; then
+    source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
+    source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+fi
 
 bindkey "^R" fzf-history-widget
 bindkey '\er' history-incremental-search-backward
