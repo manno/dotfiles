9faf2895a58b9eae13bf50a52660a430555fad71
Mario Manno <mario.manno@gmail.com>
2026-07-28 00:40:32 +0200

    Remove duplicate reload alias

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -29,7 +29,6 @@ alias h='less $HISTFILE'
 alias h1='less $HISTFILE_OLD'
 alias -g hist0='perl -a -F: -MTime::Local -ne "\$F[1]=\" \".localtime(\$F[1]); print join(\":\", @F);" $HISTFILE | less'
 alias -g hist1='perl -a -F: -MTime::Local -ne "\$F[1]=\" \".localtime(\$F[1]); print join(\":\", @F);" $HISTFILE_OLD | less'
-alias reload='source $HOME/.zshrc'
 alias -g vi!='vi `fc -n | xargs`'
 
 # git
